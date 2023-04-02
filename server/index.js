479cb8d6c98ad320915951eb5e496b2be2f85d69
Nikita_Solonets <nikita.solonets@gmail.com>
2023-04-02 13:10:06 +0300

    Presenting MVP(minimum viable product)

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
+//solving bug with latency getting signal
+var username1 = '';
+app.post('/react-app', (req, res) => {
+    var {username, password} = req.body;
+    username1 = username;
+});
+app.get('/react-app', (req, res) => {
+    res.json({message: 'welcome, '+username1});
+})
+app.get('/orange', (req, res) => {
+    res.json({message : 'Hello from the server main page Orange'});
+});
+
+app.listen(port, () => {
+    console.log(`Server is up and running at port: ${port}`);
+});
